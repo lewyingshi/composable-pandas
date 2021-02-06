95821cbaf79f77379b8cc9c167177ed9aa87d973
Dennis Lew <67657152+lewyingshi@users.noreply.github.com>
2021-02-06 16:35:33 -0600

    added new functions in feature-1

--- composable_pandas/str.py
+++ composable_pandas/str.py
@@ -1,5 +1,6 @@
 from composable import pipeable
 import pandas as pd
+import re
 
 
 @pipeable
@@ -43,3 +44,213 @@ def capitalize(col):
     
     """
     return col.str.capitalize()
+
+@pipeable
+def islower(col):
+    """Check whether all characters in each string are lowercase.
+    
+    This is equivalent to running the Python string method
+    :meth:`str.islower` for each element of the Series/Index. If a string
+    has zero characters, ``False`` is returned for that check.
+    
+    Returns
+    -------
+    Series or Index of bool
+        Series or Index of boolean values with the same length as the original
+        Series/Index.
+    
+    See Also
+    --------
+    str.isalpha : Check whether all characters are alphabetic.
+    str.isnumeric : Check whether all characters are numeric.
+    str.isalnum : Check whether all characters are alphanumeric.
+    str.isdigit : Check whether all characters are digits.
+    str.isdecimal : Check whether all characters are decimal.
+    str.isspace : Check whether all characters are whitespace.
+    str.islower : Check whether all characters are lowercase.
+    str.isupper : Check whether all characters are uppercase.
+    str.istitle : Check whether all characters are titlecase.
+    
+    Examples
+    --------
+    **Checks for Character Case**
+    
+    >>> s = pd.Series(['leopard', 'Golden Eagle', 'SNAKE', ''])
+    
+    >>> s >> islower
+    0     True
+    1    False
+    2    False
+    3    False
+    dtype: bool
+    """
+    return col.str.islower()
+
+@pipeable
+def isnumeric(col):
+    """Check whether all characters in each string are numeric.
+    
+    This is equivalent to running the Python string method
+    :meth:`str.isnumeric` for each element of the Series/Index. If a string
+    has zero characters, ``False`` is returned for that check.
+    
+    Returns
+    -------
+    Series or Index of bool
+        Series or Index of boolean values with the same length as the original
+        Series/Index.
+    
+    See Also
+    --------
+    str.isalpha : Check whether all characters are alphabetic.
+    str.isnumeric : Check whether all characters are numeric.
+    str.isalnum : Check whether all characters are alphanumeric.
+    str.isdigit : Check whether all characters are digits.
+    str.isdecimal : Check whether all characters are decimal.
+    str.isspace : Check whether all characters are whitespace.
+    str.islower : Check whether all characters are lowercase.
+    str.isupper : Check whether all characters are uppercase.
+    str.istitle : Check whether all characters are titlecase.
+    
+    Examples
+    --------
+    >>> s = pd.Series(['23', '³', '⅕', ''])
+
+        The ``s.str.isnumeric`` method is the same as ``s.str.isdigit`` but also
+    includes other characters that can represent quantities such as unicode
+    fractions.
+    
+    >>> s >> isnumeric
+    0     True
+    1     True
+    2     True
+    3    False
+    dtype: bool
+    """
+    return col.str.isnumeric()
+
+@pipeable
+def istitle(col):
+    """Check whether all characters in each string are titlecase.
+    
+    This is equivalent to running the Python string method
+    :meth:`str.istitle` for each element of the Series/Index. If a string
+    has zero characters, ``False`` is returned for that check.
+    
+    Returns
+    -------
+    Series or Index of bool
+        Series or Index of boolean values with the same length as the original
+        Series/Index.
+    
+    See Also
+    --------
+    str.isalpha : Check whether all characters are alphabetic.
+    str.isnumeric : Check whether all characters are numeric.
+    str.isalnum : Check whether all characters are alphanumeric.
+    str.isdigit : Check whether all characters are digits.
+    str.isdecimal : Check whether all characters are decimal.
+    str.isspace : Check whether all characters are whitespace.
+    str.islower : Check whether all characters are lowercase.
+    str.isupper : Check whether all characters are uppercase.
+    str.istitle : Check whether all characters are titlecase.
+    
+    Examples
+    --------
+    **Checks for Character Case**
+
+    >>> s = pd.Series(['leopard', 'Golden Eagle', 'SNAKE', ''])
+
+    The ``s.str.istitle`` method checks for whether all words are in title
+    case (whether only the first letter of each word is capitalized). Words are
+    assumed to be as any sequence of non-numeric characters separated by
+    whitespace characters.
+    
+    >>> s >> istitle
+    0    False
+    1     True
+    2    False
+    3    False
+    dtype: bool
+    """
+    return col.str.istitle()
+
+@pipeable
+def isupper(col):
+    """Check whether all characters in each string are uppercase.
+    
+    This is equivalent to running the Python string method
+    :meth:`str.isupper` for each element of the Series/Index. If a string
+    has zero characters, ``False`` is returned for that check.
+    
+    Returns
+    -------
+    Series or Index of bool
+        Series or Index of boolean values with the same length as the original
+        Series/Index.
+    
+    See Also
+    --------
+    str.isalpha : Check whether all characters are alphabetic.
+    str.isnumeric : Check whether all characters are numeric.
+    str.isalnum : Check whether all characters are alphanumeric.
+    str.isdigit : Check whether all characters are digits.
+    str.isdecimal : Check whether all characters are decimal.
+    str.isspace : Check whether all characters are whitespace.
+    str.islower : Check whether all characters are lowercase.
+    str.isupper : Check whether all characters are uppercase.
+    str.istitle : Check whether all characters are titlecase.
+    
+    Examples
+    --------
+    **Checks for Character Case**
+    
+    >>> s = pd.Series(['leopard', 'Golden Eagle', 'SNAKE', ''])
+
+    >>> s >> isupper
+    0    False
+    1    False
+    2     True
+    3    False
+    dtype: bool
+    """
+    return col.str.isupper()
+
+@pipeable
+def isspace(col):
+    """Check whether all characters in each string are alphanumeric.
+    
+    This is equivalent to running the Python string method
+    :meth:`str.isalnum` for each element of the Series/Index. If a string
+    has zero characters, ``False`` is returned for that check.
+    
+    Returns
+    -------
+    Series or Index of bool
+        Series or Index of boolean values with the same length as the original
+        Series/Index.
+    
+    See Also
+    --------
+    str.isalpha : Check whether all characters are alphabetic.
+    str.isnumeric : Check whether all characters are numeric.
+    str.isalnum : Check whether all characters are alphanumeric.
+    str.isdigit : Check whether all characters are digits.
+    str.isdecimal : Check whether all characters are decimal.
+    str.isspace : Check whether all characters are whitespace.
+    str.islower : Check whether all characters are lowercase.
+    str.isupper : Check whether all characters are uppercase.
+    str.istitle : Check whether all characters are titlecase.
+    
+    Examples
+    --------
+    **Checks for Whitespace**
+
+    >>> s = pd.Series([' ', '\\t\\r\\n ', ''])
+    >>> s >> isspace
+    0     True
+    1     True
+    2    False
+    dtype: bool
+    """
+    return col.str.isspace()
--- tests/test_str.py
+++ tests/test_str.py
@@ -1,4 +1,4 @@
-from composable_pandas.str import capitalize
+from composable_pandas.str import capitalize, islower, isnumeric, istitle, isupper, isspace
 from datetime import datetime
 
 import numpy as np
@@ -17,3 +17,62 @@ def test_capitalize():
     mixed = mixed >> capitalize()
     exp = Series(["Foo", np.nan, "Bar", np.nan, np.nan, "Blah", np.nan, np.nan, np.nan])
     tm.assert_almost_equal(mixed, exp)
+
+def test_islower():
+    values = ["A", "b", "Xy", "4", "3A", "", "TT", "55", "-", "  "]
+    str_s = Series(values)
+
+    lower_e = [False, True, False, False, False, False, False, False, False, False]
+
+    tm.assert_series_equal((str_s >> islower()), Series(lower_e))
+
+    assert (str_s >> islower()).tolist() == [v.islower() for v in values]
+
+def test_isnumeric():
+    # 0x00bc: ¼ VULGAR FRACTION ONE QUARTER
+    # 0x2605: ★ not number
+    # 0x1378: ፸ ETHIOPIC NUMBER SEVENTY
+    # 0xFF13: ３ Em 3
+    values = ["A", "3", "¼", "★", "፸", "３", "four"]
+    s = Series(values)
+    numeric_e = [False, True, True, False, True, True, False]
+    tm.assert_series_equal((s >> isnumeric()), Series(numeric_e))
+
+    unicodes = ["A", "3", "¼", "★", "፸", "３", "four"]
+    assert (s >> isnumeric()).tolist() == [v.isnumeric() for v in unicodes]
+
+    values = ["A", np.nan, "¼", "★", np.nan, "３", "four"]
+    s = Series(values)
+    numeric_e = [False, np.nan, True, False, np.nan, True, False]
+    tm.assert_series_equal((s >> isnumeric()), Series(numeric_e))
+
+def test_istitle():
+    values = ["A", "b", "Xy", "4", "3A", "", "TT", "55", "-", "  "]
+    str_s = Series(values)
+
+    title_e = [True, False, True, False, True, False, False, False, False, False]
+
+    tm.assert_series_equal((str_s >> istitle()), Series(title_e))
+
+    assert (str_s >> istitle()).tolist() == [v.istitle() for v in values]
+
+def test_isupper():
+    values = ["A", "b", "Xy", "4", "3A", "", "TT", "55", "-", "  "]
+    str_s = Series(values)
+
+    upper_e = [True, False, False, False, True, False, True, False, False, False]
+
+    tm.assert_series_equal((str_s >> isupper()), Series(upper_e))
+
+    assert (str_s >> isupper()).tolist() == [v.isupper() for v in values]
+
+def test_isspace():
+    values = ["A", "b", "Xy", "4", "3A", "", "TT", "55", "-", "  "]
+    str_s = Series(values)
+
+    space_e = [False, False, False, False, False, False, False, False, False, True]
+
+    tm.assert_series_equal((str_s >> isspace()), Series(space_e))
+
+    assert (str_s >> isspace()).tolist() == [v.isspace() for v in values]
+
